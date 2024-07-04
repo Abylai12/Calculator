9d2a68cecdf7d4f6b2b460fbc599c0f5bace2c43
Abylai12 <abilay1208@gmail.com>
2024-07-04 12:34:51 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn7 = document.getElementById("btn7");
+let screen = document.getElementById("screen");
+
+// console.log(Number(btn1.textContent));
+// screen.textContent = Number(btn1.textContent) + Number(btn1.textContent);
